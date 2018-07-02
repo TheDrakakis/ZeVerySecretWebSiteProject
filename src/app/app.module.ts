193de7122561609282f35fda3421e2412c8dc60b
Clément Garin <clement.garin@outlook.fr>
2018-07-02 22:17:23 +0200

    First version

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+import { timer } from 'rxjs/observable/timer';
+import { Location } from '@angular/common';
+
+export enum State {
+  Waiting,
+  Loading,
+  Done,
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  public cause: string;
+  public valid: boolean;
+  public states = State;
+  public loading: State;
+  public url: string;
+
+  constructor(private locationService: Location, private router: Router, private route: ActivatedRoute) {}
+
+  public submitCause(): void {
+    this.loading = State.Loading;
+
+    this.locationService.replaceState(this.formatToUrl(this.cause));
+    this.url = window.location.href;
+
+    timer(3000).subscribe(() => this.loading = State.Done);
+  }
+
+  private formatToUrl(content: string): string {
+    return encodeURIComponent(content);
+  }
+
+  private clearCause(content: string): string {
+    return content.replace(new RegExp('_', 'g'), ' ');
+  }
+
+  ngOnInit() {
+    this.loading = State.Waiting;
+    this.route.params.subscribe(params => {
+      this.cause = this.clearCause(params['id'] ? params['id'] : '');
+    });
+  }
+}
