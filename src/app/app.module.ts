65292da28dea20975806228d6a8cf1cfa9dce5fd
Clément Garin <clement.garin@outlook.fr>
2018-07-04 00:18:24 +0200

    Adding textarea height autosizing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {AutosizeModule} from 'ngx-autosize';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -16,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule,
     BrowserModule,
     FormsModule,
+    AutosizeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
